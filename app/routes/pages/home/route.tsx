f7ee16a452bf2b9ab70a436386100468bcd11f76
jin <wlsdnjs3225@naver.com>
2025-05-07 10:13:54 +0900

    히어로 애니메이션 구현

--- app/routes/pages/home/route.tsx
+++ app/routes/pages/home/route.tsx
@@ -1,9 +1,15 @@
+import Description from './components/description';
 import Hero from './components/hero';
+import HomeService from './components/home-service';
+import TransferService from './components/transfer-service';
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Description />
+      <HomeService />
+      <TransferService />
     </div>
   );
 }
